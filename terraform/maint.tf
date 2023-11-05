84912003667ead9a2c142cdc965d8a0d5e6a7df5
Tobias Sackmann <tobias.sackmann@t-online.de>
2023-11-05 18:01:00 +0100

    add k3s host

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -7,7 +7,7 @@ provider "proxmox" {
 
 resource "proxmox_vm_qemu" "traffic-host1" {
   name        = "traffic-host1"
-  desc        = "Ubuntu-Server"
+  desc        = "Ubuntu-Server for Traffic generation"
   target_node = var.proxmox_node_name
   sshkeys     = var.public_ssh_key
   agent       = 1
@@ -43,7 +43,7 @@ resource "proxmox_vm_qemu" "traffic-host1" {
 
 resource "proxmox_vm_qemu" "traffic-host2" {
   name        = "traffic-host2"
-  desc        = "Ubuntu-Server"
+  desc        = "Ubuntu-Server for Traffic generation"
   target_node = var.proxmox_node_name
   sshkeys     = var.public_ssh_key
   agent       = 1
@@ -62,6 +62,42 @@ resource "proxmox_vm_qemu" "traffic-host2" {
     storage = "local-lvm"
     type    = "scsi"
     size    = "83212M"
+    discard = "on"
+    ssd     = "1"
+  }
+  network {
+    bridge = var.ovs_bridge
+    model  = "virtio"
+  }
+  ## muss dem Template matchen
+  os_type    = "cloud-init"
+  ipconfig0  = "ip=dhcp"
+  nameserver = var.nameserver
+  ciuser     = var.user
+  cipassword = var.password
+}
+
+resource "proxmox_vm_qemu" "k3s" {
+  name        = "k3s"
+  desc        = "k3s Ubuntu-Server"
+  target_node = var.proxmox_node_name
+  sshkeys     = var.public_ssh_key
+  agent       = 1
+  clone       = var.ubuntu_cloudinit_template
+  qemu_os     = "l26"
+  # this l26 is a small l like linux
+  cores   = 2
+  sockets = 1
+  cpu     = "host"
+  memory  = 8096
+  scsihw  = "virtio-scsi-pci"
+  vga {
+    type = "std"
+  }
+  disk {
+    storage = "local-lvm"
+    type    = "scsi"
+    size    = "100G"
     discard = "on"
     ssd     = "1"
   }
