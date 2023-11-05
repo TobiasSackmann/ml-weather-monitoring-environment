ce30866a2a4b0898e16a449645a433d1c518b369
Tobias Sackmann <tobias.sackmann@t-online.de>
2023-11-05 17:33:26 +0100

    refactoring and adding variables

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Requirements
 * Proxmox is installed on your target host
 * Initial Proxmox configurations have been applied like creating an API Token
 * An Cloud Init capable image is available (is called ubuntu2204-ci in the code of this repository)
+* An OVS Bridge with name vmbr10 is available
+
+
+Usage
+-----
+* Create your .tfvars file with the variables defined in the terraform/variables.tf
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,66 @@
 variable "proxmox_api_url" {
-  type = string
+  description = "URL to the Proxmox Host API"
+  type        = string
+  default     = "https://1.2.3.4:8006/api2/json"
 }
 
 variable "proxmox_api_token_id" {
-  type      = string
-  sensitive = true
+  description = "API Token ID"
+  type        = string
+  sensitive   = true
+  default     = "example@pam!terraform"
 }
 
 variable "proxmox_api_token_secret" {
-  type      = string
-  sensitive = true
+  description = "Proxmox API Token Secret"
+  type        = string
+  sensitive   = true
+  default     = "somereall-ygoo-dsec-ret0-foofoofoofoo"
+}
+
+variable "public_ssh_key" {
+  description = "SSH Key to be copied to the created virtual machines"
+  type        = string
+}
+
+variable "ubuntu_cloudinit_template" {
+  description = "Template from an Cloud image"
+  type        = string
+  sensitive   = true
+  default     = "ubuntu2204-ci"
+}
+
+variable "proxmox_node_name" {
+  description = "The target node of your Proxmox Cluster"
+  type        = string
+  sensitive   = true
+  default     = "proxmox"
+}
+
+variable "ovs_bridge" {
+  description = "The target OVS bridge"
+  type        = string
+  sensitive   = true
+  default     = "vmbr10"
+}
+
+variable "user" {
+  description = "The user that will be created on the VMS"
+  type        = string
+  sensitive   = true
+  default     = "user"
+}
+
+variable "password" {
+  description = "The Password for the created VM user"
+  type        = string
+  sensitive   = true
+  default     = "password"
+}
+
+variable "nameserver" {
+  description = "The name server for the new VMs"
+  type        = string
+  sensitive   = true
+  default     = "8.8.8.8"
 }
