62bcab6e70aca6ccd6a0767b09d3f37df2562ff0
Tobias Sackmann <tobias.sackmann@t-online.de>
2023-11-07 21:08:13 +0100

    improve output generation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
+# prepare new setup
+rm -f host_ip.csv
+
+# start testbed creation by creating VMs with terraform
+terraform -chdir=./terraform/ init
 terraform -chdir=./terraform/ apply -auto-approve
+
+# Use terraform outputs to generate Anisble inventory
 traffichost1_ip=$(terraform output -state=./terraform/terraform.tfstate traffic-host1_ip)
 traffichost2_ip=$(terraform output -state=./terraform/terraform.tfstate traffic-host2_ip)
 k3s_ip=$(terraform output -state=./terraform/terraform.tfstate k3s_ip)
 
-echo "traffic-host1,${traffichost1_ip}" >> host_ip.csv
-echo "traffic-host2,${traffichost2_ip}" >> host_ip.csv
-echo "k3s,${k3s_ip}" >> host_ip.csv
+traffichost1_name=$(terraform output -state=./terraform/terraform.tfstate traffic-host1_name)
+traffichost2_name=$(terraform output -state=./terraform/terraform.tfstate traffic-host2_name)
+k3s_name=$(terraform output -state=./terraform/terraform.tfstate k3s_name)
+
+echo "${traffichost1_name},${traffichost1_ip}" >> host_ip.csv
+echo "${traffichost2_name},${traffichost2_ip}" >> host_ip.csv
+echo "${k3s_name},${k3s_ip}" >> host_ip.csv
--- terraform/output.tf
+++ terraform/output.tf
@@ -3,12 +3,27 @@ output "traffic-host1_ip" {
   sensitive = true
 }
 
+output "traffic-host1_name" {
+  value     = proxmox_vm_qemu.traffic-host1.name
+  sensitive = true
+}
+
 output "traffic-host2_ip" {
   value     = proxmox_vm_qemu.traffic-host2.ssh_host
   sensitive = true
 }
 
+output "traffic-host2_name" {
+  value     = proxmox_vm_qemu.traffic-host2.name
+  sensitive = true
+}
+
 output "k3s_ip" {
   value     = proxmox_vm_qemu.k3s.ssh_host
+  sensitive = true
+}
+
+output "k3s_name" {
+  value     = proxmox_vm_qemu.k3s.name
   sensitive = true
 }
