4aa281fc8a152f2d1e75b3af7ee87cb16c7d856b
Tobias Sackmann <tobias.sackmann@t-online.de>
2023-11-07 20:49:43 +0100

    fix setup.sh output generation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 terraform -chdir=./terraform/ apply -auto-approve
-terraform output -state=./terraform/terraform.tfstate traffic-host1_ip >> ip.log
-terraform output -state=./terraform/terraform.tfstate traffic-host2_ip >> ip.log
-terraform output -state=./terraform/terraform.tfstate k3s_ip >> ip.log
+traffichost1_ip=$(terraform output -state=./terraform/terraform.tfstate traffic-host1_ip)
+traffichost2_ip=$(terraform output -state=./terraform/terraform.tfstate traffic-host2_ip)
+k3s_ip=$(terraform output -state=./terraform/terraform.tfstate k3s_ip)
+
+echo "traffic-host1,${traffichost1_ip}" >> host_ip.csv
+echo "traffic-host2,${traffichost2_ip}" >> host_ip.csv
+echo "k3s,${k3s_ip}" >> host_ip.csv
