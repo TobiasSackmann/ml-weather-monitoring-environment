b8ef978b32142188cbe01cf58ef361807052ce00
Tobias Sackmann <tobias.sackmann@t-online.de>
2024-07-14 16:18:11 +0200

    remove unneeded vms

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,22 +1,22 @@
-output "traffic-host1_ip" {
-  value     = proxmox_vm_qemu.traffic-host1.ssh_host
-  sensitive = true
-}
+# output "traffic-host1_ip" {
+#   value     = proxmox_vm_qemu.traffic-host1.ssh_host
+#   sensitive = true
+# }
 
-output "traffic-host1_name" {
-  value     = proxmox_vm_qemu.traffic-host1.name
-  sensitive = true
-}
+# output "traffic-host1_name" {
+#   value     = proxmox_vm_qemu.traffic-host1.name
+#   sensitive = true
+# }
 
-output "traffic-host2_ip" {
-  value     = proxmox_vm_qemu.traffic-host2.ssh_host
-  sensitive = true
-}
+# output "traffic-host2_ip" {
+#   value     = proxmox_vm_qemu.traffic-host2.ssh_host
+#   sensitive = true
+# }
 
-output "traffic-host2_name" {
-  value     = proxmox_vm_qemu.traffic-host2.name
-  sensitive = true
-}
+# output "traffic-host2_name" {
+#   value     = proxmox_vm_qemu.traffic-host2.name
+#   sensitive = true
+# }
 
 output "k3s_ip" {
   value     = proxmox_vm_qemu.k3s.ssh_host
